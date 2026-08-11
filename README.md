d65b24c03154c7af42b6dd1ec64ac744092cb95c
Allen <allencch@hotmail.com>
2026-08-12 01:05:46 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Todo List
+
+This is a simple Todo List web application.
+
+## Requirement
+
+`asdf` (or `mise`). Refer to `.tool-versions` involved.
+
+## Stack
+
+Backend
+- NodeJS
+- Fastify
+
+Database
+- PostgreSQL
+- Drizzle
+
+Frontend
+- Vue
+- Tailwind
+- Vite
---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify';
+import 'dotenv/config';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/api/health', async () => {
+  return { status: 'ok' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
