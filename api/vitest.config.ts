78b04bbd66fbe8516987a08e622008aae6d3bc33
Allen <allencch@hotmail.com>
2026-08-12 14:12:06 +0800

    test(api): fix vitest config

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'vitest/config';
 import path from 'path';
 
 export default defineConfig({
-  test: {
-    setupFiles: ['./tests/setup.ts'],
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
+    setupFiles: ['./tests/setup.ts'],
     globals: true,
     environment: 'node',
     include: ['tests/**/*.test.ts'],
